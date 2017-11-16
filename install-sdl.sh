16f5443e79636c84a51de2e6f48a1df6d9c9ad42
Akos Balogh <balogh.akos.jr@gmail.com>
2017-11-16 18:08:13 +0100

    basic sdl 1.2 install script added

---
+++ install-sdl.sh
@@ -0,0 +1,27 @@
+##
+#  This bash script installs Hoembrew and SDL 1.2 for macOS.
+##
+
+
+# INSTALLING HOMEBREW (if it isn't installed yet)
+
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+
+# INSTALLING SDL 1.2
+
+echo "Installing SDL 1.2 and: sdl_gfx  sdl_ttf  sdl_image  sdl_mixes"
+
+brew install sdl  sdl_gfx  sdl_ttf  sdl_image  sdl_mixes
+## if any error hapens, try: brew install --force  
+
+
+
+
+
